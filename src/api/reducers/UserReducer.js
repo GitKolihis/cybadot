cf8fcd3eef317acf566f22223da535db03acfe39
Adelani Olanrewaju Israel <adelaniolanrewajuisrael@gmail.com>
2023-02-14 02:20:55 +0100

    Building

--- src/api/reducers/UserReducer.js
+++ src/api/reducers/UserReducer.js
@@ -1,16 +1,62 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import data from "../../../data";
+
+let datas = [
+  {
+    name: "Israel",
+    age: 21,
+    job: "frontend developer",
+    country: "France",
+    hobby: "Reading",
+    id: 1,
+  },
+  {
+    name: "David",
+    age: 22,
+    job: "backend developer",
+    country: "Germany",
+    hobby: "Gist",
+    id: 2,
+  },
+  {
+    name: "Michael",
+    age: 35,
+    job: "backend developer",
+    country: "UK",
+    hobby: "Football",
+    id: 3,
+  },
+  {
+    name: "Frank",
+    age: 24,
+    job: "mobile developer",
+    country: "USA",
+    hobby: "Reading",
+    id: 4,
+  },
+  {
+    name: "Xavi",
+    age: 26,
+    job: "devops",
+    country: "Spain",
+    hobby: "Movies",
+    id: 5,
+  },
+];
 
 export const getUsers = createAsyncThunk("allusers", async () => {
-  let res;
-  res = {
-    code: 200,
-    message: "User retrieved successfully",
-    data: data,
-  };
-  console.log("Users Data: ", res);
-  return res;
+  try {
+    let res;
+    res = {
+      code: 200,
+      message: "User retrieved successfully",
+      data: datas,
+    };
+    console.log("Users Data: ", res);
+    return res;
+  } catch (e) {
+    console.log("Get users error: ", e);
+  }
 });
 
 export const UserSlice = createSlice({
@@ -43,6 +89,6 @@ export const UserSlice = createSlice({
   },
 });
 
-export const { usersCleanup} = UserSlice.actions
+export const { usersCleanup } = UserSlice.actions;
 
 export default UserSlice.reducer;
